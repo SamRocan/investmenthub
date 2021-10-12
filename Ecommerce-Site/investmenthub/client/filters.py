587d109d82d08124e02b189d32e4cee393475c09
SamRocan <SamClendenan@iCloud.com>
2021-10-12 08:17:43 +0100

    Added more sophisticated Product Filter / Search

--- Ecommerce-Site/investmenthub/client/filters.py
+++ Ecommerce-Site/investmenthub/client/filters.py
@@ -1,8 +1,13 @@
 import django_filters
-
+from django_filters import DateFilter, CharFilter
 from product.models import *
 
 class ProductFilter(django_filters.FilterSet):
+    created = DateFilter(field_name="date_added", lookup_expr='gte')
+    description = CharFilter(field_name="description", lookup_expr='icontains')
+    #title = CharFilter(field_name="title", lookup_expr='icontains')
     class Meta:
         model = Product
-        fields = ['title', 'research_type', 'slug', 'description', 'price']
+        #fields = ['title', 'research_type', 'slug', 'description', 'price']
+        fields = '__all__'
+        exclude = 'file'
