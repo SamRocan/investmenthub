27890e1942d2d528bd23597367509ac168f556bb
SamRocan <SamClendenan@iCloud.com>
2021-10-13 08:51:48 +0100

    Cart App Created

--- Ecommerce-Site/investmenthub/investmenthub/settings.py
+++ Ecommerce-Site/investmenthub/investmenthub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'main',
     'client',
     'product',
+    'cart',
     'django_filters',
     'crispy_forms',
 ]
--- Ecommerce-Site/investmenthub/investmenthub/urls.py
+++ Ecommerce-Site/investmenthub/investmenthub/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('client/', include('client.urls')),
     path('', include('main.urls')),
-    path('', include('product.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('product.urls')),
+    path('', include('cart.urls'))
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
