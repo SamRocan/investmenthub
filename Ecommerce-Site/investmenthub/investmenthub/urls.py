632230a58e6deed8840a215557e35c965699cee4
SamRocan <SamClendenan@iCloud.com>
2021-10-13 09:54:12 +0100

    Context processor created for cart + cart logic

--- Ecommerce-Site/investmenthub/investmenthub/urls.py
+++ Ecommerce-Site/investmenthub/investmenthub/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('client/', include('client.urls')),
+    path('cart/', include('cart.urls')),
     path('', include('main.urls')),
-    path('', include('product.urls')),
-    path('', include('cart.urls'))
+    path('', include('product.urls'))
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
