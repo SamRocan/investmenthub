a8ba61c03eb3816c431c15d6604954850e7d831b
SamRocan <SamClendenan@iCloud.com>
2021-10-13 06:35:25 +0100

    Forms Stylized

--- Ecommerce-Site/investmenthub/client/filters.py
+++ Ecommerce-Site/investmenthub/client/filters.py
@@ -3,18 +3,18 @@ from django_filters import DateFilter, CharFilter
 from product.models import *
 
 class UserProductFilter(django_filters.FilterSet):
-    description = CharFilter(field_name="description", lookup_expr='icontains')
-    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
-    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    description = CharFilter(label="Name", field_name="description", lookup_expr='icontains')
+    price__gt = django_filters.NumberFilter(label="Min Price",field_name='price', lookup_expr='gt')
+    price__lt = django_filters.NumberFilter(label="Max Price", field_name='price', lookup_expr='lt')
     class Meta:
         model = Product
         fields = ''
         exclude = ['file','client', 'slug']
 
 class ProductFilter(django_filters.FilterSet):
-    description = CharFilter(field_name="description", lookup_expr='icontains')
-    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
-    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    description = CharFilter(label="Name", field_name="description", lookup_expr='icontains')
+    price__gt = django_filters.NumberFilter(label="Min Price",field_name='price', lookup_expr='gt')
+    price__lt = django_filters.NumberFilter(label="Max Price", field_name='price', lookup_expr='lt')
     class Meta:
         model = Product
         fields = ''
