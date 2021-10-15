d2f5e1c414ae4d9f0b7d90fdbba2f78819d07fc8
SamRocan <SamClendenan@iCloud.com>
2021-10-15 12:40:45 +0100

    Email sent with product attached after purchase

--- Ecommerce-Site/investmenthub/cart/views.py
+++ Ecommerce-Site/investmenthub/cart/views.py
@@ -3,10 +3,13 @@ from django.views.generic import View
 from django.http import JsonResponse
 from .cart import Cart
 
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 # Create your views here.
 def cart_detail(request):
     cart = Cart(request)
-
     remove_from_cart = request.GET.get('remove_from_cart', '')
     if remove_from_cart:
         cart.remove(remove_from_cart)
@@ -15,9 +18,28 @@ def cart_detail(request):
     return render(request, 'cart/cart.html',)
 
 class OrderCompleted(View):
-    def get(self,request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         cart = Cart(request)
+        #Send Email from here
+        current_user = request.user
+        send_email(cart, current_user.email)
+        print(current_user.email)
         cart.clear()
         context = {
         }
         return JsonResponse(context, safe=False)
+
+
+def send_email(cart, email):
+    for i in cart:
+        file = i['product'].file.path
+        email = EmailMessage(
+            str(i['product'].title)+': Ready to Download',
+            'Thank you for purchasing ' + str(i['product'].title) + 'please find you file attached',
+            settings.DEFAULT_FROM_EMAIL,
+            [email],
+            reply_to=[settings.DEFAULT_FROM_EMAIL],
+            headers={'Message-ID': 'foo'},
+        )
+        email.attach_file(file)
+        email.send()
--- Ecommerce-Site/investmenthub/investmenthub/settings.py
+++ Ecommerce-Site/investmenthub/investmenthub/settings.py
@@ -141,3 +141,12 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+DEFAULT_FROM_EMAIL = 'sam.rocan@gmail.com'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sam.rocan@gmail.com'
+EMAIL_HOST_PASSWORD = 'toyhpoenhngbqbkd'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
