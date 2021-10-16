16fdab265bee55c7d08b5185b6cf0b1ad4bf32b4
SamRocan <SamClendenan@iCloud.com>
2021-10-16 13:48:46 +0100

    Extended Sign Up form - includes first name, last name, and email

--- Ecommerce-Site/investmenthub/client/views.py
+++ Ecommerce-Site/investmenthub/client/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.text import slugify
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 
