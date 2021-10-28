9514b36fb714999d4bfae704447beccd92b189ec
SamRocan <SamClendenan@iCloud.com>
2021-10-28 18:27:21 +0100

    Tweaked forms styling - Added extra css files to modularise code - Added specific attributes to the forms.py

--- Ecommerce-Site/investmenthub/product/forms.py
+++ Ecommerce-Site/investmenthub/product/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,TextInput, Textarea, FileField
 
 from .models import Product, ProductFile
 
@@ -6,7 +6,28 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['title', 'research_type', 'slug', 'description', 'price', 'file']
-
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Name'
+            }),
+            'research_type': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Research Type'
+            }),
+            'slug': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Product Slug (no spaces)'
+            }),
+            'description': Textarea(attrs={
+                'class':"form-control",
+                'style':'max-width: 300px; min-height:100px;',
+                'placeholder':'Describe the product here...'
+            }),
+        }
 class ProductImageForm(ModelForm):
     class Meta:
         model = ProductFile
