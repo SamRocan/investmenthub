0d26aba9886c214d78cd53d12090d12a8a22433a
SamRocan <SamClendenan@iCloud.com>
2021-10-28 22:15:05 +0100

    Sign Up and Login Forms Sylized - forms.py adjusted to assist in styling

--- Ecommerce-Site/investmenthub/client/forms.py
+++ Ecommerce-Site/investmenthub/client/forms.py
@@ -1,15 +1,41 @@
 from django import forms
+from django.forms import TextInput, PasswordInput
+
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 
 class UserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, label='Email')
-    first_name = forms.CharField(max_length=255, label='First Name')
-    last_name = forms.CharField(max_length=255, label='Last Name')
     class Meta:
         model = User
         fields = ("first_name", "last_name","username", "email", "password1", "password2")
+        widgets = {
+            'first_name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'First Name'
+            }),
+            'last_name': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Last Name'
+            }),
+            'username': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Username'
+            }),
+            'email': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;',
+                'placeholder': 'Email'
+            })
+        }
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget = PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
 
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
@@ -19,3 +45,19 @@ class UserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control',
+               'style': 'max-width: 300px;',
+               'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'style': 'max-width: 300px;',
+            'placeholder': 'Password'
+        }
+    ))
--- Ecommerce-Site/investmenthub/client/urls.py
+++ Ecommerce-Site/investmenthub/client/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .forms import UserLoginForm
 from . import views
 urlpatterns = [
     path('client_admin/', views.client_admin, name='client_admin'),
@@ -10,5 +11,5 @@ urlpatterns = [
     path('graphs/', views.ChartData.as_view(), name='ChartData'),
     path('register/', views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(template_name='client/logout.html'), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='client/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='client/login.html', authentication_form=UserLoginForm), name='login')
 ]
